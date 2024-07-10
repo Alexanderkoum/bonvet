debb9e155ccbbec37574f2af4d20b22e4c83ccde
Alexanderkoum <mbappekoum@gmail.com>
2024-07-09 22:08:43 -0400

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Logo from "./logo";
+import Logo from "./Logo";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 import twitter from '../assets/twitter.png'
